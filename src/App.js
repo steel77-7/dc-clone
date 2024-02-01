5bc22c6c87957bf4be34be89a903d91e05c58f29
steel77-7 <utkarshkaundal1970@gmail.com>
2024-02-01 22:55:57 +0530

    commit before uploading

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
 import React from 'react'
 
-import logo from './logo.svg';
 import './App.css';
 import Server_navbar from './components/the_home_page/Server_navbar';
 import Dmlist from './components/the_home_page/Dmlist'
-import TopNavHome from './components/the_home_page/TopNavHome'
+
 
 import MainAreaHome from './components/the_home_page/MainAreaHome';
 function App() {
@@ -15,12 +14,7 @@ function App() {
         <div className='flex flex-row '>
           <Dmlist />
           <MainAreaHome />
-          
         </div>
-          {/* <Dmlist />
-          <TopNavHome /> */}
-         
-        
       </div>
     </>
   );
---
+++ src/components/the_home_page/TheFriendArea.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+import Server_navbar from './components/the_home_page/Server_navbar';
+import Dmlist from './components/the_home_page/Dmlist'
+
+
+import MainAreaHome from './components/the_home_page/MainAreaHome';
+
+export default function TheFriendArea(){
+
+return(
+    <>
+       
+    </>
+)
+
+}
